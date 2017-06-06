68db9dec7e7a19e9f06b21ec2363eafc88d5b8f1
Hugo Trippaers <hugo@trippaers.nl>
2017-06-06 20:28:45 +0200

    Configure the skeleton

---
+++ src/main/java/nl/sonicity/sha2017/cms/cmshabackend/CmshaBackendApplication.java
@@ -0,0 +1,12 @@
+package nl.sonicity.sha2017.cms.cmshabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmshaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmshaBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/sonicity/sha2017/cms/cmshabackend/CmshaBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.sonicity.sha2017.cms.cmshabackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmshaBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
