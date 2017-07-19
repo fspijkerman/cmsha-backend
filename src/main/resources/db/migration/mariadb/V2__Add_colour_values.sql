c5759eaed20caf688c7978246b35dfeb1046508f
Hugo Trippaers <htrippaers@schubergphilis.com>
2017-07-19 23:04:16 +0200

    Fix type is sql script

--- src/main/resources/db/migration/mariadb/V2__Add_colour_values.sql
+++ src/main/resources/db/migration/mariadb/V2__Add_colour_values.sql
@@ -1,4 +1,4 @@
 alter table `active_claim`
     add column `red` FLOAT NOT NULL,
     add column `green` FLOAT NOT NULL,
-    add column `blue` FLOAT NOT NULL,;
+    add column `blue` FLOAT NOT NULL;
